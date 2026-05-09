764587d0ee1227760758959d8664480a1827840c
Jim Myers <jim.myers@jfmyers.dev>
2026-05-09 13:27:35 -0700

    chore: silence SDK ABI build warning

--- build.rs
+++ build.rs
@@ -1,10 +1,4 @@
-use std::path::Path;
-
 fn main() {
     println!("cargo:rerun-if-env-changed=WOOTING_RGB_SDK_PATH");
     println!("cargo:rerun-if-changed=external/wooting-rgb-sdk/src/wooting-rgb-sdk.h");
-
-    if Path::new("external/wooting-rgb-sdk").exists() {
-        println!("cargo:warning=Using Wooting RGB SDK ABI from external/wooting-rgb-sdk");
-    }
 }
